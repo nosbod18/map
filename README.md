563844ed3e72669a39b35fd96f8d6e48e3614e99
Evan Dobson <evan.dobson18@gmail.com>
2023-06-28 14:46:05 -0500

    Made single-header

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # map
+An extremely fast hash table with robin hood based hashing.
 
 ```c
+#define MAP_IMPL
 #include "map.h"
 #include <string.h>
+#include <stdio.h>
 
 uint64_t hash(void const *key, size_t stride, uint64_t seed) {
     uint64_t hash = 5381;
@@ -16,16 +19,25 @@ bool equal(void const *a, void const *b) {
 }
 
 int main(void) {
-    Map m = MAP(char *, char *, hash, equal, 0, 0);
+    map_t m = MAP(char *, char *, hash, equal, 0, 0);
     if (!map_init(&m)) return 1;
 
-    map_set(&m, "Hello", "World");
-    char **world = map_get(&m, "Hello"); // *world == "World"
+    char const *key = "Hello";
+    char const *val = "World";
+    char **get = NULL;
 
-    map_rem(&m, "Hello");
-    world = map_get(&m, "Hello"); // world == NULL
+    map_set(&m, &key, &val);
+    get = map_get(&m, &key);
+    printf("Hello = %s\n", get ? *get : "<null>");
+
+    map_rem(&m, &key);
+    get = map_get(&m, &key);
+    printf("%s = %s\n", key, get ? *get : "<null>");
 
     map_free(&m);
 }
 
+// Output:
+//  Hello = World
+//  Hello = <null>
 ```
--- map.h
+++ map.h
@@ -1,3 +1,8 @@
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Interface
+///
+
 #pragma once
 #include <stdbool.h>
 #include <stddef.h>
@@ -9,13 +14,160 @@ typedef struct {
     bool (*equal)(void const *, void const *);
     size_t cap, count, stride, key_stride, val_stride;
     uint64_t seed;
-} Map;
+} map_t;
 
 #define MAP(K, V, HASH, EQUAL, CAP, SEED)\
-    (Map){.key_stride = sizeof (K), .val_stride = sizeof (V), .hash = (HASH), .equal = (EQUAL), .cap = (CAP), .seed = (SEED)}
+    (map_t){.key_stride = sizeof (K), .val_stride = sizeof (V), .hash = (HASH), .equal = (EQUAL), .cap = (CAP), .seed = (SEED)}
 
-bool  map_init (Map *m);
-bool  map_set  (Map *m, void const *key, void const *val);
-void *map_get  (Map *m, void const *key);
-bool  map_rem  (Map *m, void const *key);
-void  map_free (Map *m);
+bool  map_init (map_t *m);
+bool  map_set  (map_t *m, void const *key, void const *val);
+void *map_get  (map_t *m, void const *key);
+bool  map_rem  (map_t *m, void const *key);
+void  map_free (map_t *m);
+
+///////////////////////////////////////////////////////////////////////////////
+///
+/// Implementation
+///
+
+#if defined(MAP_IMPL)
+
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    uint64_t hash: 56;
+    uint64_t dist: 8;
+} _map_slot_t;
+
+static _map_slot_t *_map_get_slot(map_t *m, size_t i) {
+    return (_map_slot_t *)&((char *)m->slots)[i * m->stride];
+}
+
+static void _map_load_kv(map_t *m, void const *key, void const *val) {
+    _map_slot_t *item = m->item;
+    item->hash = m->hash(key, m->key_stride, m->seed) & 0x00ffffffffffffff;
+    item->dist = 1;
+    memcpy(item + 1, key, m->key_stride);
+    memcpy((char *)(item + 1) + m->key_stride, val, m->val_stride);
+}
+
+static bool resize(map_t *m, size_t cap) {
+    map_t m2 = *m;
+    m2.cap = cap;
+    if (!map_init(&m2))
+        return false;
+
+    for (size_t i = 0; i < m->cap; i++) {
+        _map_slot_t *item = _map_get_slot(m, i);
+        if (item->dist == 0) continue;
+        item->dist = 1;
+        for (size_t j = item->hash & (m2.cap - 1); ; item->dist++, j = (j + 1) & (m2.cap - 1)) {
+            _map_slot_t *slot = _map_get_slot(&m2, j);
+            if (slot->dist == 0) {
+                memcpy(slot, item, m->stride);
+                break;
+            }
+            if (slot->dist < item->dist) {
+                memcpy(m->swap, slot, m->stride);
+                memcpy(slot, item, m->stride);
+                memcpy(item, m->swap, m->stride);
+            }
+        }
+    }
+
+    free(m->slots);
+    *m = m2;
+    return true;
+}
+
+bool map_init(map_t *m) {
+    if (m->key_stride == 0 || m->val_stride == 0 || m->hash == NULL || m->equal == NULL)
+        return false;
+
+    size_t cap = 16;
+    for (; cap < m->cap; cap *= 2);
+
+    m->cap = cap;
+    m->stride = m->key_stride + m->val_stride + sizeof (_map_slot_t);
+    m->slots = calloc(m->cap + 2, m->stride);
+
+    if (m->slots == NULL)
+        return false;
+
+    m->item = _map_get_slot(m, m->cap);
+    m->swap = _map_get_slot(m, m->cap + 1);
+    return true;
+}
+
+bool map_set(map_t *m, void const *key, void const *val) {
+    if (m->count * 4 == m->cap * 3)
+        if (!resize(m, m->cap * 2))
+            return NULL;
+
+    _map_load_kv(m, key, val);
+    _map_slot_t *item = m->item;
+
+    for (size_t i = item->hash & (m->cap - 1); ; item->dist++, i = (i + 1) & (m->cap - 1)) {
+        _map_slot_t *slot = _map_get_slot(m, i);
+        if (slot->dist == 0 || (slot->hash == item->hash && m->equal(slot + 1, key))) {
+            m->count += (slot->dist == 0);
+            memcpy(slot, item, m->stride);
+            return true;
+        }
+        if (slot->dist < item->dist) {
+            memcpy(m->swap, slot, m->stride);
+            memcpy(slot, item, m->stride);
+            memcpy(item, m->swap, m->stride);
+        }
+    }
+}
+
+void *map_get(map_t *m, void const *data) {
+    uint64_t hash = m->hash(data, m->key_stride, m->seed) & 0x00ffffffffffffff;
+    for (size_t i = hash & (m->cap - 1); ; i = (i + 1) & (m->cap - 1)) {
+        _map_slot_t *slot = _map_get_slot(m, i);
+        if (slot->dist == 0)
+            return NULL;
+        if (slot->hash == hash && m->equal(slot + 1, data))
+            return slot + 1;
+    }
+}
+
+bool map_rem(map_t *m, void const *data) {
+    uint64_t hash = m->hash(data, m->key_stride, m->seed) & 0x00ffffffffffffff;
+    for (size_t i = hash & (m->cap - 1); ; i = (i + 1) & (m->cap - 1)) {
+        _map_slot_t *slot = _map_get_slot(m, i);
+        if (slot->dist == 0)
+            return false;
+        if (slot->hash == hash && m->equal(slot + 1, data)) {
+            slot->dist = 0;
+            for (;;) {
+                _map_slot_t *prev = slot;
+                i = (i + 1) & (m->cap - 1);
+                slot = _map_get_slot(m, i);
+                if (slot->dist <= 1) {
+                    prev->dist = 0;
+                    break;
+                }
+                memcpy(prev, slot, m->stride);
+                prev->dist--;
+            }
+            m->count--;
+            if (m->count * 10 == m->cap)
+                resize(m, m->cap / 2);
+            return true;
+        }
+    }
+}
+
+void map_free(map_t *m) {
+    if (m == NULL)
+        return;
+
+    free(m->slots);
+    m->cap = m->count = 0;
+    m->item = m->swap = NULL;
+}
+
+#endif // MAP_IMPL
